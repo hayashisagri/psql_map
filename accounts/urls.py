3806362b4ff4d6c6edae9d81484b8ead9f5360d5
hayashisagri <113956610+hayashisagri@users.noreply.github.com>
2022-10-18 10:44:54 +0900

    emailを使用したユーザー認証に変更

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,17 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+from accounts.forms import EmailAuthenticationForm
 from .views import SignupView
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("signup/", SignupView.as_view(), name="signup"),
+    path('login/', LoginView.as_view(
+        form_class=EmailAuthenticationForm,
+        redirect_authenticated_user=True,
+        template_name='accounts/login.html'
+    ), name='login')
 ]
--- mapproject/settings.py
+++ mapproject/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -14,7 +13,6 @@ SECRET_KEY = 'django-insecure-^c_*x^v7&-ztv$+55q(1=dly#=r0kfldj1c_ctdm!2v_ry$9*!
 DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0']
-
 
 # Application definition
 
@@ -59,7 +57,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mapproject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -73,7 +70,6 @@ DATABASES = {
         'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -93,7 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -106,7 +101,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -124,3 +118,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "mypage"
 LOGOUT_REDIRECT_URL = "home"
+
+AUTHENTICATION_BACKENDS = [
+    'accounts.backends.EmailAuthenticationBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
