fbf629747cc08769d817cdb317ba6c9249be276f
hayashisagri <113956610+hayashisagri@users.noreply.github.com>
2022-10-24 12:47:55 +0900

    fix

--- prefecture/views.py
+++ prefecture/views.py
@@ -159,7 +159,4 @@ def mypage_view(request):
 def prefecture_list(request):
     prefectures = Prefecture.objects.filter(user=request.user)
     serializer = PrefectureSerializer(prefectures, many=True)
-    if request.method == 'GET':
-        return Response(serializer.data)
-    else:
-        Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.data)
